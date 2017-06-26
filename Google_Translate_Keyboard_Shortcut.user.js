7323aa2c3d75ff0ce6a9326a62f9aa7a907e8639
Paweł Golonko <pgolonko@gmail.com>
2017-06-26 21:18:52 +0200

    Update user script

---
+++ Google_Translate_Keyboard_Shortcut.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Google Translate Keyboard Shortcut
+// @namespace    https://github.com/Greenek
+// @version      1.1
+// @description  Press ctrl-s to swap active languages
+// @author       Paweł Golonko
+// @license      MIT; https://opensource.org/licenses/MIT
+// @copyright    2017, Paweł Golonko (http://greenek.com)
+// @homepageURL  https://github.com/Greenek/google-translate-keyboard-shortcut-userscript
+// @supportURL   https://github.com/Greenek/google-translate-keyboard-shortcut-userscript/issues
+// @icon         https://raw.githubusercontent.com/Greenek/google-translate-keyboard-shortcut-userscript/master/icon.png
+// @updateURL    https://raw.githubusercontent.com/Greenek/google-translate-keyboard-shortcut-userscript/master/google-translate-keyboard-shortcut.user.js
+// @downloadURL  https://raw.githubusercontent.com/Greenek/google-translate-keyboard-shortcut-userscript/master/google-translate-keyboard-shortcut.user.js
+// @grant        none
+// @run-at       document-end
+// @include      http://translate.google.tld/*
+// @include      https://translate.google.tld/*
+// ==/UserScript==
+(() => {
+  'use strict';
+  
+  // Get swap button element
+  const swapBtn = document.getElementById('gt-swap');
+
+  /**
+   * Swap languages by emulating mouse click events on UI button.
+   */
+  function swapLanguages() {
+    ['mouseover', 'mousedown', 'mouseup'].forEach(type =>
+      swapBtn.dispatchEvent(new MouseEvent(type))
+    );
+  }
+
+  /**
+   * Set listeners for shortcut event.
+   */
+  window.addEventListener(
+    'keypress',
+    event => {
+      if (event.key === 's' && event.ctrlKey) {
+        swapLanguages();
+      }
+    },
+    true
+  );
+})();
